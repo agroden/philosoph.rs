9fb02f3d9582f1991cf11358a9a413a8732c9e1f
Alexander Groden <alexander.groden@gmail.com>
2021-04-11 21:08:41 -0400

    better input validation

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,29 @@ use clap::{Arg,App};
 
 
 
+
 fn main() {
+	fn validate_pholosophers(x: String) -> Result<(), String> {
+		let val: u8;
+		match x.trim().parse::<u8>() {
+			Ok(n) => val = n,
+			Err(_e) => return Err(format!("Expected a number between 2 and 255, not \"{}\".", x))
+		}
+		if val >= 2 && val <= std::u8::MAX { return Ok(()); }
+		return Err(format!("Expected a number between 2 and 255, not \"{}\".", x));
+	}
+	// collect args
 	let args = App::new("philosoph.rs")
 		.version(env!("CARGO_PKG_VERSION"))
 		.about("Implementation of the Chandy/Misra solution to Dijkstra's dining philosphers problem.")
 		.arg(Arg::with_name("PHILOSOPHERS")
-			.help("The number of philosopher's at the table")
+			.help(&format!("The number of philosopher's at the table, between 2 and {}", std::u8::MAX))
 			.default_value("5")
 			.index(1)
-			.validator(|x| match x.parse::<u32>() {
-				Ok(_n) => Ok(()),
-				Err(_e) => Err(String::from(format!("Expected an unsigned integer, not \"{}\".", x)))
-			})
+			.validator(validate_pholosophers)
 		).get_matches();
-	let num = value_t!(args, "PHILOSOPHERS", u32).unwrap();
-
+	let num = value_t!(args, "PHILOSOPHERS", u8).unwrap();
+	// create philosophers
 	let mut philosophers = Vec::new();
 	for i in 0..num {
 		let t = thread::spawn(move || {
