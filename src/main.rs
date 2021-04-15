857afa7ad23a8f414b4387cdf67f8a24758fb69e
Alexander Groden <alexander.groden@gmail.com>
2021-04-15 08:38:07 -0400

    randomly generate wait times

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,27 @@ extern crate clap;
 extern crate rand;
 
 use std::{env, thread, time};
+use rand::{thread_rng, Rng};
 use clap::{Arg,App};
 
 
-fn philosopher(id: u8, cycles: u8) {
+fn philosopher(id: u8, cycles: u8, delay: u8) {
 	println!("id: {}, name: {}, cycles: {}", id, thread::current().name().unwrap(), cycles);
-	// alternate think and eat
-	let mut sentinel = cycles;
+	let mut rng = thread_rng();
+
+	let mut sentinel = cycles + 1;
 	while sentinel > 0 {
 		// request forks
-		let eat_time: u8 = rand::random();
-		println!("eating for {} seconds", eat_time);
+		let eat_time: u8 = rng.gen_range(0..delay);
+		println!("{}: eating for {} seconds", id, eat_time);
 		thread::sleep(time::Duration::from_secs(eat_time.into()));
 
 		// mark forks as dirty
-		let think_time: u8 = rand::random();
-		println!("thinking for {} seconds", think_time);
+		let think_time: u8 = rng.gen_range(0..delay);
+		println!("{}: thinking for {} seconds", id, think_time);
 		thread::sleep(time::Duration::from_secs(think_time.into()));
 
-		sentinel -= 1;
+		if cycles != 0 {	sentinel -= 1; }
 	}
 }
 
@@ -51,18 +53,24 @@ fn main() {
 			.validator(|x| valid_u8(x, Some(2)))
 		)
 		.arg(Arg::with_name("cycles")
-			.help("The number of cycles of thinking and eating a philosopher will do before leaving")
+			.help("The number of cycles of thinking and eating a philosopher will do before leaving (0..255), 0 is cycle infinitely")
 			.default_value("0")
+			.validator(|x| valid_u8(x, None))
+		)
+		.arg(Arg::with_name("max_delay")
+			.help("The maximum think and eat time in seconds of each philosopher (0..255)")
+			.default_value("10")
 			.validator(|x| valid_u8(x, None))
 		).get_matches();
 	let total_philosophers = value_t!(args, "PHILOSOPHERS", u8).unwrap();
 	let cycles = value_t!(args, "cycles", u8).unwrap();
+	let delay = value_t!(args, "max_delay", u8).unwrap();
 
 	// create philosophers
 	let mut philosophers = Vec::new();
 	for i in 0..total_philosophers {
 		let builder = thread::Builder::new().name(format!("Philosopher #{}", i));
-		let t = builder.spawn(move || philosopher(i, cycles)).unwrap();
+		let t = builder.spawn(move || philosopher(i, cycles, delay)).unwrap();
 		philosophers.push(t);
 	}
 	for p in philosophers {
